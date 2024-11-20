1620b001d8a4d905798ec19b434b14c682d04062
sabinaismailova <sabinaismailova.2003@gmail.com>
2024-11-20 16:01:20 -0500

    fix build errors

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/portfolio",
+  output: "export",
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   webpack(config) {
     return config;
   },
-  output: 'export',
 };
 
 export default nextConfig;
